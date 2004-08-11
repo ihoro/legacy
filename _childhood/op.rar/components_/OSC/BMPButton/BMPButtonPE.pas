e2f54cc3518db20cad22192941a5e7b52b1b916b
Igor Ostapenko <ihoro@users.noreply.github.com>
2004-08-11 14:03:58 +0300

    op.rar:components_/OSC/BMPButton/BMPButtonPE.pas

---
+++ _childhood/op.rar/components_/OSC/BMPButton/BMPButtonPE.pas
@@ -0,0 +1,35 @@
+unit BMPButtonPE;
+
+interface
+
+uses
+  SysUtils, DesignEditors, ExtDlgs, Dialogs, Forms;
+
+type
+  TBMPButtonPE = class(TStringProperty)
+    function GetAttributes: TPropertyAttributes; override;
+    procedure Edit; override;
+  end;
+
+implementation
+
+function TBMPButtonPE.GetAttributes: TPropertyAttributes;
+begin
+  Result:=[paDialog]
+end;
+
+procedure TBMPButtonPE.Edit;
+var
+  OpenBMP: TOpenPictureDialog;
+begin
+  OpenBMP:=TOpenPictureDialog.Create(Application);
+  try
+    OpenBMP.Filter:='Bitmap files|*.bmp';
+    if OpenBMP.Execute then
+      SetStrValue(OpenBMP.FileName)
+  finally
+    OpenBMP.Free
+  end
+end;
+
+end.
