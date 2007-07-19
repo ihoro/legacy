015c66495cd6e1af91ba94c6b1498e393ac42f64
Igor Ostapenko <ihoro@users.noreply.github.com>
2007-07-19 16:50:06 +0000

    Import of 'comstuff' project.

---
+++ comstuff/trunk/src/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by comstuff.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MAIN_DIALOG                 101
+#define IDC_PORT                        1003
+#define IDC_SPEED                       1005
+#define IDC_PARITY                      1006
+#define IDC_STOPBITS                    1007
+#define IDC_TIME                        1008
+#define IDC_START                       1009
+#define IDC_TIME_LEFT                   1010
+#define IDC_BUTTON1                     1011
+#define IDC_ABOUT                       1011
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
