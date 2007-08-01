a17012ef59072bcd26f51c961d384b9a2f235b3f
Igor Ostapenko <ihoro@users.noreply.github.com>
2007-08-01 10:41:22 +0000

    Added manifest for using xp-style.

--- comstuff/trunk/src/stdafx.h
+++ comstuff/trunk/src/stdafx.h
@@ -42,3 +42,5 @@
 
 
 // TODO: reference additional headers your program requires here
+
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
